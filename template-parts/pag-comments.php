8845cb6c34b624ac6e275bdeacf270108315b914
Henry Wright <henrywright@gmail.com>
2016-07-11 21:07:42 +0100

    Add space to pagination links

--- template-parts/pag-comments.php
+++ template-parts/pag-comments.php
@@ -8,9 +8,7 @@
 
 if ( get_comment_pages_count() > 1 ) { ?>
 	<nav role="navigation">
-		<?php
-		previous_comments_link( __( 'Previous', 'octothorpe' ) );
-		next_comments_link( __( 'Next', 'octothorpe' ) );
-		?>
+		<?php previous_comments_link( __( 'Previous', 'octothorpe' ) ); ?>
+		<?php next_comments_link( __( 'Next', 'octothorpe' ) ); ?>
 	</nav>
 <?php }
--- template-parts/pag-posts.php
+++ template-parts/pag-posts.php
@@ -9,9 +9,7 @@
 global $wp_query;
 if ( $wp_query->max_num_pages > 1 ) { ?>
 	<nav role="navigation">
-		<?php
-		previous_posts_link( __( 'Previous', 'octothorpe' ) );
-		next_posts_link( __( 'Next', 'octothorpe' ) );
-		?>
+		<?php previous_posts_link( __( 'Previous', 'octothorpe' ) ); ?>
+		<?php next_posts_link( __( 'Next', 'octothorpe' ) ); ?>
 	</nav>
 <?php }
