161553e738147fa4889b062cc7571f5a96fc5c08
Henry Wright <henrywright@gmail.com>
2016-07-13 14:40:42 +0100

    Use double quotes

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
 ?>
 
 <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label for='s'><?php esc_html_e( 'Search', 'octothorpe' ); ?></label>
+	<label for="s"><?php esc_html_e( 'Search', 'octothorpe' ); ?></label>
 	<input type="search" name="s" id="s" value="<?php echo esc_attr( get_search_query() ); ?>">
 </form>
