d66db9fbfafd28af9254873df03911b56dcc7bf9
Henry Wright <henrywright@gmail.com>
2016-12-15 13:30:05 +0000

    Do not escape search query

--- searchform.php
+++ searchform.php
@@ -9,5 +9,5 @@
 
 <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label for="s"><?php esc_html_e( 'Search', 'octothorpe' ); ?></label>
-	<input type="search" name="s" id="s" value="<?php echo esc_attr( get_search_query() ); ?>">
+	<input type="search" name="s" id="s" value="<?php echo get_search_query(); ?>">
 </form>
