a26d0e3e337e397dc97a5767265008dcda56574a
Henry Wright <henrywright@gmail.com>
2017-01-04 02:11:55 +0000

    Add a submit button to the search form

--- searchform.php
+++ searchform.php
@@ -10,4 +10,5 @@
 <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label for="s"><?php esc_html_e( 'Search', 'octothorpe' ); ?></label>
 	<input type="search" name="s" id="s" value="<?php echo get_search_query(); ?>">
+	<input type="submit" value="<?php esc_attr_e( 'Search', 'octothorpe' ); ?>">
 </form>
