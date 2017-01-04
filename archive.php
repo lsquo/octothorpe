836870f5c1d739582a81cd1fbb754483cf8e6da7
Henry Wright <henrywright@gmail.com>
2017-01-04 15:05:19 +0000

    Add ARIA landmarks to archive, home and search

--- archive.php
+++ archive.php
@@ -6,26 +6,28 @@
  * @subpackage Templates
  */
 
-get_header();
-the_archive_title( '<header><h1>', '</h1></header>' );
-the_archive_description();
-if ( have_posts() ) : ?>
-	<ul class="post-list">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<li <?php post_class(); ?>>
-				<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
-				<?php the_title(
-					sprintf(
-						'<a href="%1$s">',
-						esc_url( get_permalink() )
-					),
-					'</a>'
-				); ?>
-			</li>
-		<?php endwhile; ?>
-	</ul>
-	<?php get_template_part( 'template-parts/pag', 'posts' );
-else : ?>
-	<p><?php esc_html_e( 'No posts found.', 'octothorpe' ); ?></p>
-<?php endif;
-get_footer();
+get_header(); ?>
+<main role="main">
+	<?php the_archive_title( '<header><h1>', '</h1></header>' );
+	the_archive_description();
+	if ( have_posts() ) : ?>
+		<ul class="post-list">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<li <?php post_class(); ?>>
+					<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
+					<?php the_title(
+						sprintf(
+							'<a href="%1$s">',
+							esc_url( get_permalink() )
+						),
+						'</a>'
+					); ?>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+		<?php get_template_part( 'template-parts/pag', 'posts' );
+	else : ?>
+		<p><?php esc_html_e( 'No posts found.', 'octothorpe' ); ?></p>
+	<?php endif; ?>
+</div>
+<?php get_footer();
--- home.php
+++ home.php
@@ -7,27 +7,29 @@
  */
 
 get_header(); ?>
-<header>
-	<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
-</header>
-<p><?php echo esc_html( get_bloginfo( 'description' ) ); ?></p>
-<?php if ( have_posts() ) : ?>
-	<ul class="post-list">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<li <?php post_class(); ?>>
-				<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
-				<?php the_title(
-					sprintf(
-						'<a href="%1$s">',
-						esc_url( get_permalink() )
-					),
-					'</a>'
-				); ?>
-			</li>
-		<?php endwhile; ?>
-	</ul>
-	<?php get_template_part( 'template-parts/pag', 'posts' );
-else : ?>
-	<p><?php esc_html_e( 'No posts found.', 'octothorpe' ); ?></p>
-<?php endif;
-get_footer();
+<main role="main">
+	<header>
+		<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
+	</header>
+	<p><?php echo esc_html( get_bloginfo( 'description' ) ); ?></p>
+	<?php if ( have_posts() ) : ?>
+		<ul class="post-list">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<li <?php post_class(); ?>>
+					<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
+					<?php the_title(
+						sprintf(
+							'<a href="%1$s">',
+							esc_url( get_permalink() )
+						),
+						'</a>'
+					); ?>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+		<?php get_template_part( 'template-parts/pag', 'posts' );
+	else : ?>
+		<p><?php esc_html_e( 'No posts found.', 'octothorpe' ); ?></p>
+	<?php endif; ?>
+</div>
+<?php get_footer();
